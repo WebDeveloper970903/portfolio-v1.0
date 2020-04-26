1eae177fec95a04e5acaf57ff2c0abdee8d8c67a
alexandreformoso <alexandre_formoso@hotmail.com>
2020-04-26 22:13:01 +0100

    Add CSS Animattions, BEM, NPM, SASS, Custom Grids

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// Calculating the vh
+
+// 1. get the v unit (viewport height * 1%) 
+let vh = window.innerHeight * 0.01;
+// set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`); 
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
